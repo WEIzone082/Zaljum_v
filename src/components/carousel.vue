44c5391e9d6ee0d15444946f0bc89631447c4ad9
WEIzone082 <wtfuhell@gmail.com>
2022-02-01 23:42:57 +0800

    update

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="carousel">
-    <div class="slider p3" ref="p3" @click="show">
+    <div class="slider p3" ref="p3" @click="show(3)">
       <div class="overlay"></div>
     </div>
-    <div class="slider p2" ref="p2">
+    <div class="slider p2" ref="p2" @click="show(2)">
       <div class="overlay hidden_3"></div>
     </div>
-    <div class="slider p1" ref="p1">
+    <div class="slider p1" ref="p1" @click="show(1)">
       <div class="overlay hidden_2"></div>
     </div>
-    <div class="slider p0" ref="p0">
+    <div class="slider p0" ref="p0" @click="show(0)">
       <div class="overlay hidden_1"></div>
     </div>
   </div>
@@ -24,51 +24,56 @@ export default {
       hidden: 3,
       drift: 1,
       slides: null,
+      msg: "hey",
     };
   },
   methods: {
-    show: function () {
-      const page = "this.$refs[this.slides[this.page]]";
-      if (this.dir) {
-        eval(page).classList.toggle("drift_" + String(this.drift));
-        eval(page).firstChild.classList.toggle("hidden_" + String(this.hidden));
-        this.page--;
-        if (this.drift != 3) {
-          this.drift++;
-          this.hidden--;
-        }
-        eval(page).firstChild.className = "overlay";
-        if (this.page == 0) {
-          this.dir = !this.dir;
-        }
-      } else {
-        eval(page).firstChild.classList.toggle("hidden_" + String(this.hidden));
-        this.page++;
-        if (this.page != 0) {
-          eval(page).classList.toggle("drift_" + String(this.drift));
-        }
-        if (this.drift != 1) {
-          this.drift--;
-          this.hidden++;
-        }
-        eval(page).firstChild.className = "overlay";
-        if (this.page == 3) {
-          this.dir = !this.dir;
-        }
-      }
+    show: function (num) {
+      console.log(this.$refs);
     },
   },
 
   mounted() {
-    this.slides = Object.keys(this.$refs).reverse();
-
-    // window.setInterval(function () {
-    //   const page = "this.$refs[this.slides[this.page]]";
-    //   if (this.dir) {
-    //     eval(page).classList.toggle("drift_" + String(page));
-    //     eval(page).classList.toggle("hidden_" + String(page));
-    //   }
-    // }, 4000);
+    setTimeout(() => {
+      window.setInterval(() => {
+        this.slides = Object.keys(this.$refs).reverse();
+        if (this.dir) {
+          this.$refs[this.slides[this.page]].classList.toggle(
+            "drift_" + String(this.drift)
+          );
+          this.$refs[this.slides[this.page]].firstChild.classList.toggle(
+            "hidden_" + String(this.hidden)
+          );
+          this.page--;
+          if (this.drift != 3) {
+            this.drift++;
+            this.hidden--;
+          }
+          this.$refs[this.slides[this.page]].firstChild.className = "overlay";
+          if (this.page == 0) {
+            this.dir = !this.dir;
+          }
+        } else {
+          this.$refs[this.slides[this.page]].firstChild.classList.toggle(
+            "hidden_" + String(this.hidden)
+          );
+          this.page++;
+          if (this.page != 0) {
+            this.$refs[this.slides[this.page]].classList.toggle(
+              "drift_" + String(this.drift)
+            );
+          }
+          if (this.drift != 1) {
+            this.drift--;
+            this.hidden++;
+          }
+          this.$refs[this.slides[this.page]].firstChild.className = "overlay";
+          if (this.page == 3) {
+            this.dir = !this.dir;
+          }
+        }
+      }, 4000);
+    }, 4000);
   },
 };
 </script>
@@ -83,3 +88,5 @@ export default {
   }
 }
 </style>
+
+// unable to use eval in mounted()
